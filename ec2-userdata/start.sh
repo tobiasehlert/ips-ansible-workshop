c88366161680914444da6ae5a689a4a44b408c3d
Tobias Lindberg <tobias.ehlert@gmail.com>
2020-02-05 18:12:05 +0000

    updating startup script with yum and not dnf

--- ec2-userdata/start.sh
+++ ec2-userdata/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-yum install ansible git-core -y
-ansible-pull -U https://github.com/tobiasehlert/ips-ansible-workshop/ec2-userdata start.yml
+yum install git-core -y
+amazon-linux-extras install ansible2
+ansible-pull -U https://github.com/tobiasehlert/ips-ansible-workshop ec2-userdata/start.yml
