08e5147f1658d114756ba0b2093c9d1410e9bbb6
Tobias Lindberg <tobias.ehlert@gmail.com>
2020-02-05 21:37:43 +0000

    updating generate-hosts to be a bit better..

--- tools/generate-hosts.sh
+++ tools/generate-hosts.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # short script to get public dns name and ansible-workshop tag..
 # then you need to do manual stuff, to build a nice working hosts.yml
-aws ec2 describe-instances --output text --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[PublicDnsName, [Tags[?Key==`ansible-workshop`].Value] [0][0] ]' > hosts-tmp.yml
-
+echo 'all:
+  hosts:' > hosts-tmp.yml
+aws ec2 describe-instances --output text --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[PublicDnsName, [Tags[?Key==`ansible-workshop`].Value] [0][0] ]' | grep Master  | awk '{print "    " $1}' >> hosts-tmp.yml
+echo '  children:
+    webservers:
+      hosts:' >> hosts-tmp.yml
+aws ec2 describe-instances --output text --filters Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[PublicDnsName, [Tags[?Key==`ansible-workshop`].Value] [0][0] ]' | grep -v Master | awk '{print "        " $1}' >> hosts-tmp.yml
+mv hosts-tmp.yml ../hosts.yml
